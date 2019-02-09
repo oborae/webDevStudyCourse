61c1c7bc79d935bac24954bfbcfd82dc57f7eeba
Omar El Borae <omar.elborae@gmail.com>
2019-02-09 20:21:59 +0300

    JS

---
+++ 5- Java Script/10_Stalker_Solution/script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+
+var age = prompt("How old are you?");
+
+alert("Your Full Name is " + firstName + " " + lastName);
+console.log("Your Full Name is " + firstName + " " + lastName);
+alert("Your Age is " + age);
+console.log("Your Age is " + age);
---
+++ 5- Java Script/12_Age_Calc_Solution/myscript.js
@@ -0,0 +1,5 @@
+var age = prompt("How old are you?");
+
+var days = age * 365;
+
+console.log("you've been alive for " + days + " days");
