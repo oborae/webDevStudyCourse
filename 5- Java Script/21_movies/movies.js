4c9941104731a9ed7689c656b0766ed6acb9049d
Omar El Borae <omar.elborae@gmail.com>
2019-02-13 21:44:52 +0300

    JS Movies

--- 5- Java Script/21_movies/movies.js
+++ 5- Java Script/21_movies/movies.js
@@ -16,6 +16,17 @@ var movies =  [
 	}
 ];
 
-for (var i = 0; i < Things.length; i++) {
-	Things[i]
+for (var i = 0; i < movies.length; i++) {
+	if(movies[i].watched == true)
+		console.log("You have watched the movie \"" + movies[i].name + "\" and it's rating is - " + movies[i].rating + " stars.");
+	else
+		console.log("You haven't watched the movie \"" + movies[i].name + "\" and it's rating is - " + movies[i].rating + " stars.");
 }
+
+movies.forEach(function(movie){
+	if(movie.watched == true)
+		console.log("You have watched the movie \"" + movie.name + "\" and it's rating is - " + movie.rating + " stars.");
+	else
+		console.log("You haven't watched the movie \"" + movie.name + "\" and it's rating is - " + movie.rating + " stars.");
+
+});
